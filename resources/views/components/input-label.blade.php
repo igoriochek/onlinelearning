c6ebf6cf35dfc6d05b50c9a304012a368c793e79
samomari <marius.sam98@gmail.com>
2025-09-04 10:48:43 +0300

    Add prettier configuration

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,9 @@
-@props(['value'])
+@props([
+  'value',
+])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
-    {{ $value ?? $slot }}
+<label
+	{{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}
+>
+	{{ $value ?? $slot }}
 </label>
