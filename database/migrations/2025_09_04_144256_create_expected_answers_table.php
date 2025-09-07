2ed6d6192f5d801f509ff577117a9ab1e2224b00
samomari <marius.sam98@gmail.com>
2025-09-07 18:07:21 +0300

    Format code with prettier

--- database/migrations/2025_09_04_144256_create_expected_answers_table.php
+++ database/migrations/2025_09_04_144256_create_expected_answers_table.php
@@ -4,26 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('expected_answers', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('step_id')->constrained('steps')->onDelete('cascade');
-            $table->string('expected_answer');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('expected_answers', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('step_id')->constrained('steps')->onDelete('cascade');
+			$table->string('expected_answer');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('expected_answers');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('expected_answers');
+	}
 };
