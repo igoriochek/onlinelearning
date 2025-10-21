43aada7d9b6d5e29567c90cc85fc5978205d85ed
samomari <marius.sam98@gmail.com>
2025-10-21 23:51:15 +0300

    Add more helper icons

---
+++ resources/views/components/icons/drag-handle.blade.php
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="w-5 h-5"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke="currentColor"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		d="M8 9h.01M8 15h.01M12 9h.01M12 15h.01M16 9h.01M16 15h.01"
+	/>
+</svg>
